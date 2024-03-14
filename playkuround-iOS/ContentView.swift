12120dade47558d97ad69cd81f5d21c0e34a1e55
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-14 21:40:58 +0900

    first commit

---
+++ playkuround-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 3/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ playkuround-iOS/playkuround_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  playkuround_iOSApp.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct playkuround_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
