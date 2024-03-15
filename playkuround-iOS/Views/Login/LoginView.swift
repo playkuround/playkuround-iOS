abdf065b3424bc1e24018717551c3c6eb74cdbdf
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-16 01:40:16 +0900

    [Design] #6 - LoginView 초기화면 UI 구현

--- playkuround-iOS/Views/Login/LoginView.swift
+++ playkuround-iOS/Views/Login/LoginView.swift
@@ -12,8 +12,31 @@ struct LoginView: View {
         ZStack {
             Color.kuBackground.ignoresSafeArea(.all)
             
-            VStack {
+            VStack(alignment: .leading) {
+                Text(StringLiterals.Login.hello)
+                    .font(.neo24)
+                    .foregroundStyle(.kuText)
+                    .kerning(-0.41)
+                    .padding(.bottom, 10)
+                
+                Text(StringLiterals.Login.description)
+                    .font(.pretendard15R)
+                    .foregroundStyle(.kuText)
+                    .padding(.bottom, 48)
+                
+                Image(.longButtonWhite)
+                
+                Image(.longButtonGray)
+                    .overlay {
+                        Text(StringLiterals.Login.requestCode)
+                            .font(.neo15)
+                            .foregroundStyle(.kuText)
+                            .kerning(-0.41)
+                    }
+                
+                Spacer()
             }
+            .padding(.top, 80)
         }
     }
 }
