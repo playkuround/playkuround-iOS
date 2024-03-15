ede13aa61d82283fc858bb273b6866a4e4a4f83a
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-16 00:28:17 +0900

    [Feat] #3 - Button 클릭 시 LoginView로 전환되도록 구현

--- playkuround-iOS/Views/Main/MainView.swift
+++ playkuround-iOS/Views/Main/MainView.swift
@@ -9,33 +9,37 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        ZStack {
-            Image(.mainBackground)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .ignoresSafeArea(.all)
-            
-            VStack {
-                Text(StringLiterals.Main.introduction)
-                    .font(.neo20)
-                    .foregroundStyle(.kuText)
-                    .kerning(-0.41)
-                    .padding(.top, 80)
+        NavigationStack {
+            ZStack {
+                Image(.mainBackground)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .ignoresSafeArea(.all)
                 
-                Image(.mainLogo)
-                    .padding(.top, 18)
-                
-                Spacer()
-                
-                Image(.shortButtonBlue)
-                    .overlay {
-                        Text(StringLiterals.Main.login)
-                            .font(.neo20)
-                            .foregroundStyle(.kuText)
-                            .kerning(-0.41)
+                VStack {
+                    Text(StringLiterals.Main.introduction)
+                        .font(.neo20)
+                        .foregroundStyle(.kuText)
+                        .kerning(-0.41)
+                        .padding(.top, 80)
+                    
+                    Image(.mainLogo)
+                        .padding(.top, 18)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: LoginView()) {
+                        Image(.shortButtonBlue)
+                            .overlay {
+                                Text(StringLiterals.Main.login)
+                                    .font(.neo20)
+                                    .foregroundStyle(.kuText)
+                                    .kerning(-0.41)
+                            }
+                            .padding(.bottom, 98)
                     }
-                    .padding(.bottom, 98)
+                }
             }
         }
     }
