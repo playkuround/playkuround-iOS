9d0050d0e016b989f2080d0fdc006b2ac81638d5
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-15 22:12:28 +0900

    [Setting] #1 - 폴더링 정리

---
+++ playkuround-iOS/Views/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 3/15/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
