ffaff7a3d0e29054bcf413d96ad43fe4f58ba66b
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-15 22:08:15 +0900

    [Add] #1 - StringLiterals 파일 추가 및 세팅

---
+++ playkuround-iOS/Resources/StringLiterals.swift
@@ -0,0 +1,19 @@
+//
+//  StringLiterals.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 3/15/24.
+//
+
+import SwiftUI
+
+enum StringLiterals {
+    enum Main {
+        static let login = "로그인"
+    }
+    
+    enum Login {
+        static let description = "안녕하세요!\n건국대학교 이메일을 인증해주세요."
+        static let requestCode = "인증코드 요청"
+    }
+}
