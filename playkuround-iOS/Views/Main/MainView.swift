0f6fcf433a6ef2225787be2dc4e474de20c5392f
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-16 00:08:02 +0900

    [Add] #3 - MainView 파일 및 필요한 이미지 에셋 추가

---
+++ playkuround-iOS/Views/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 3/16/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
