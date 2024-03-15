3fa9ff5df83d2057de58f0ebf1ff2b4d73957cbc
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-15 21:52:25 +0900

    [Setting] #1 - Font 파일 추가 및 info.plist 세팅

---
+++ playkuround-iOS/Resources/Font.swift
@@ -0,0 +1,17 @@
+//
+//  Font.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 3/15/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static let neo15: Font = .custom("NeoDunggeunmoPro-Regular", size: 15)
+    static let neo18: Font = .custom("NeoDunggeunmoPro-Regular", size: 18)
+    static let neo20: Font = .custom("NeoDunggeunmoPro-Regular", size: 20)
+    static let neo22: Font = .custom("NeoDunggeunmoPro-Regular", size: 22)
+    static let neo50: Font = .custom("NeoDunggeunmoPro-Regular", size: 50)
+    static let pretendard15R: Font = .custom("Pretendard-Regular", size: 15)
+}
