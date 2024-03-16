4f26e055289441d8b1e70fe3976fec4b65e29ca2
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-16 11:05:28 +0900

    [Chore] #7 - backgroundImages를 enum으로 관리

--- playkuround-iOS/Views/Main/MainView.swift
+++ playkuround-iOS/Views/Main/MainView.swift
@@ -7,20 +7,27 @@
 
 import SwiftUI
 
+// enum으로 배경 이미지 관리
+enum mainBackgroundImage: String, CaseIterable {
+    case mainBackground1
+    case mainBackground2
+    case mainBackground3
+    case mainBackground4
+}
+
 struct MainView: View {
-    private let backgroundImages = ["mainBackground1", "mainBackground2", "mainBackground3", "mainBackground4"]
-    let timer = Timer.publish(every: 0.7, on: .main, in: .common).autoconnect()
+    private let backgroundImages = mainBackgroundImage.allCases
+    private let timer = Timer.publish(every: 0.7, on: .main, in: .common).autoconnect()
     @State private var currentIndex = 0
     
     var body: some View {
         NavigationStack {
             ZStack {
-                Image(backgroundImages[currentIndex])
+                Image(backgroundImages[currentIndex].rawValue)
                     .ignoresSafeArea(.all)
                     .onReceive(timer) { _ in
                         self.currentIndex = (self.currentIndex + 1) % self.backgroundImages.count
                     }
-                    
                 
                 VStack {
                     Text(StringLiterals.Main.introduction)
