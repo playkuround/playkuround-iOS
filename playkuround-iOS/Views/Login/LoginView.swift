690440eb5dc79fa69378c898b17eba7e1cb5bb19
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-17 01:07:56 +0900

    [Design] #6 - 인증코드뷰 기본 UI 디자인 구현

--- playkuround-iOS/Views/Login/LoginView.swift
+++ playkuround-iOS/Views/Login/LoginView.swift
@@ -11,6 +11,11 @@ struct LoginView: View {
     @State private var userId: String = ""
     @FocusState private var focusField: Bool
     
+    @State private var requestButtonName = StringLiterals.Login.requestCode
+    @State private var isButtonClicked = false
+    
+    @State private var userRequestCode: String = ""
+    
     var body: some View {
         ZStack {
             Color.kuBackground.ignoresSafeArea(.all)
@@ -43,9 +48,42 @@ struct LoginView: View {
                             )
                     }
                 
-                Image(userId.isEmpty ? .longButtonGray : .longButtonBlue)
+                
+                Image(isButtonClicked || userId.isEmpty ? "longButtonGray" : "longButtonBlue")
+                    .onTapGesture {
+                        isButtonClicked.toggle()
+                        if isButtonClicked {
+                            // 클릭되었을 때, 텍스트와 이미지 변경
+                            requestButtonName = StringLiterals.Login.reRequestCode
+                        } else {
+                            // 클릭 해제되었을 때, 원래 텍스트로 변경
+                            requestButtonName = StringLiterals.Login.requestCode
+                        }
+                    }
                     .overlay {
-                        Text(StringLiterals.Login.requestCode)
+                        Text(requestButtonName)
+                            .font(.neo15)
+                            .foregroundStyle(.kuText)
+                            .kerning(-0.41)
+                    }
+                
+                Text(StringLiterals.Login.goEmail)
+                    .font(.pretendard12R)
+                    .foregroundStyle(.kuDarkBlue).underline()
+                    .padding(.top, 6)
+                
+                Image(.longButtonWhite)
+                    .overlay {
+                        TextField(StringLiterals.Login.code, text: $userRequestCode)
+                            .font(.pretendard15R)
+                            .kerning(-0.41)
+                            .padding(.leading, 20)
+                    }
+                    .padding(.top, 46)
+                
+                Image(.longButtonGray)
+                    .overlay {
+                        Text(StringLiterals.Login.certification)
                             .font(.neo15)
                             .foregroundStyle(.kuText)
                             .kerning(-0.41)
