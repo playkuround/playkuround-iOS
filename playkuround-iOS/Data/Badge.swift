453c3394667f38a03bdc56e345121104fba31fb4
Hoeun Lee <hoeunlee228@gmail.com>
2024-03-16 14:42:08 +0900

    [Setting] #9 - Badge enum 구조화

---
+++ playkuround-iOS/Data/Badge.swift
@@ -0,0 +1,63 @@
+//
+//  Badge.swift
+//  playkuround-iOS
+//
+//  Created by Hoeun Lee on 3/16/24.
+//
+
+/// Badge 구조화, 이름은 뱃지 코드, RawValue는 뱃지 설명
+enum Badge: String {
+    // 출석 관련 뱃지
+    case ATTENDANCE_1 = "첫 출석"
+    case ATTENDANCE_5 = "5회 출석"
+    case ATTENDANCE_10 = "10회 출석"
+    case ATTENDANCE_30 = "30회 출석"
+    case ATTENDANCE_50 = "50회 출석"
+    case ATTENDANCE_100 = "100회 출석"
+
+    // 기념일 뱃지
+    case ATTENDANCE_FOUNDATION_DAY = "05월 15일 개교 기념일에 출석"
+    case ATTENDANCE_ARBOR_DAY = "04월 05일 식목일에 출석"
+    case ATTENDANCE_CHILDREN_DAY = "05월 05일 어린이날에 출석"
+    case ATTENDANCE_WHITE_DAY = "03월 14일 화이트데이에 출석"
+    case ATTENDANCE_DUCK_DAY = "05월 02일 오리데이에 출석"
+
+    // 대학별 뱃지
+    case COLLEGE_OF_LIBERAL_ARTS = "문과대학 1회 이상 탐험"
+    case COLLEGE_OF_SCIENCES = "이과대학 1회 이상 탐험"
+    case COLLEGE_OF_ARCHITECTURE = "건축대학 1회 이상 탐험"
+    case COLLEGE_OF_ENGINEERING = "공과대학 1회 이상 탐험"
+    case COLLEGE_OF_SOCIAL_SCIENCES = "사회과학대학 1회 이상 탐험"
+    case COLLEGE_OF_BUSINESS_ADMINISTRATION = "경영대학 1회 이상 탐험"
+    case COLLEGE_OF_REAL_ESTATE = "부동산과학원 1회 이상 탐험"
+    case COLLEGE_OF_INSTITUTE_TECHNOLOGY = "융합과학기술원 1회 이상 탐험"
+    case COLLEGE_OF_BIOLOGICAL_SCIENCES = "생명과학대학 1회 이상 탐험"
+    case COLLEGE_OF_VETERINARY_MEDICINE = "수의과대학 1회 이상 탐험"
+    case COLLEGE_OF_ART_AND_DESIGN = "예술디자인대학 1회 이상 탐험"
+    case COLLEGE_OF_EDUCATION = "사범대학 1회 이상 탐험"
+
+    // 경영대 특별 뱃지
+    case COLLEGE_OF_BUSINESS_ADMINISTRATION_10 = "경영대학 10회 이상 탐험"
+    case COLLEGE_OF_BUSINESS_ADMINISTRATION_30 = "경영대학 30회 이상 탐험"
+    case COLLEGE_OF_BUSINESS_ADMINISTRATION_50 = "경영대학 50회 이상 탐험"
+    case COLLEGE_OF_BUSINESS_ADMINISTRATION_70 = "경영대학 70회 이상 탐험"
+    case COLLEGE_OF_BUSINESS_ADMINISTRATION_100_AND_FIRST_PLACE = "경영대학 100회 이상 탐험 및 1등 달성"
+
+    // 예디대 특별 뱃지
+    case COLLEGE_OF_ART_AND_DESIGN_BEFORE_NOON = "예술디자인대학 09:00 ~ 11:59 탐험"
+    case COLLEGE_OF_ART_AND_DESIGN_AFTER_NOON = "예술디자인대학 12:00 ~ 18:00 탐험"
+    case COLLEGE_OF_ART_AND_DESIGN_NIGHT = "예술디자인대학 23:00 ~ 04:00 탐험"
+
+    // 공대 특별 뱃지
+    case COLLEGE_OF_ENGINEERING_A = "공대 A동 10회 이상 탐험"
+    case COLLEGE_OF_ENGINEERING_B = "공대 B동 10회 이상 탐험"
+    case COLLEGE_OF_ENGINEERING_C = "공대 C동 10회 이상 탐험"
+
+    // 스토리용 뱃지
+    case THE_DREAM_OF_DUCK = "스토리 컷씬 마스터"
+
+    // 월간랭킹 관련 뱃지
+    case MONTHLY_RANKING_1 = "월간 랭킹 1등"
+    case MONTHLY_RANKING_2 = "월간 랭킹 2등"
+    case MONTHLY_RANKING_3 = "월간 랭킹 3등"
+}
