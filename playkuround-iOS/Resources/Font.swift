c89d20394708e5b68c3db03d62edc721d9400078
Hoeun Lee <hoeunlee228@gmail.com>
2024-03-20 23:58:20 +0900

    [Add] #16 - 이용약관 뷰 제목용 새 서식(.neo38) 폰트 추가

--- playkuround-iOS/Resources/Font.swift
+++ playkuround-iOS/Resources/Font.swift
@@ -12,6 +12,7 @@ extension Font {
     static let neo18: Font = .custom("NeoDunggeunmoPro-Regular", size: 18)
     static let neo20: Font = .custom("NeoDunggeunmoPro-Regular", size: 20)
     static let neo22: Font = .custom("NeoDunggeunmoPro-Regular", size: 22)
+    static let neo38: Font = .custom("NeoDunggeunmoPro-Regular", size: 38)
     static let neo50: Font = .custom("NeoDunggeunmoPro-Regular", size: 50)
     static let pretendard15R: Font = .custom("Pretendard-Regular", size: 15)
 }
