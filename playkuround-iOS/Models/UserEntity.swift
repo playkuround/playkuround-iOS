b6d04b7b1e036ac3fe9635cc7dbc4c7c06758701
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-03-30 02:44:17 +0900

    [Add] #41 - MyPageViewProfileView에 쓰이는 UserEntity파일 생성

---
+++ playkuround-iOS/Models/UserEntity.swift
@@ -0,0 +1,16 @@
+//
+//  UserEntity.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 3/30/24.
+//
+
+import Foundation
+
+struct UserEntity {
+    var nickname: String
+    var major: String
+    var myRank: MyRank
+    var highestScore: Int
+    var highestRank: String
+}
