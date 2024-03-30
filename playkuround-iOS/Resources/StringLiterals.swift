823df63332fcda17826b72bdabc4d281fb0e8956
Hoeun Lee <hoeunlee228@gmail.com>
2024-03-31 02:28:39 +0900

    [Chore] #56 - 컨벤션 맞추어 코드 수정 및 주석 추가

--- playkuround-iOS/Resources/StringLiterals.swift
+++ playkuround-iOS/Resources/StringLiterals.swift
@@ -116,6 +116,7 @@ enum StringLiterals {
     enum Game {
         static let play = "계속하기"
         static let home = "홈"
+        static let timerTitle = "TIME"
         
         enum Time {
             static let title = "10초를 맞춰봐"
--- playkuround-iOS/Views/Games/TimerBarView.swift
+++ playkuround-iOS/Views/Games/TimerBarView.swift
@@ -12,7 +12,7 @@ struct TimerBarView: View {
     
     var body: some View {
         HStack(alignment: .bottom) {
-            Text("TIME")
+            Text(StringLiterals.Game.timerTitle)
                 .font(.neo22)
                 .foregroundStyle(.white)
                 .kerning(-0.41)
@@ -31,7 +31,10 @@ struct TimerBarView: View {
                     
                     Rectangle()
                         .frame(width: 272 * progress, height: 8)
-                        .foregroundColor(progress >= 0.5 ? .kuTimebarGreen : (progress >= 0.2 ? .kuTimebarOrange : .kuTimebarRed))
+                        // 50% 이상 초록, 20% 이상 주황, 20% 미만 빨강
+                        .foregroundColor(progress >= 0.5 ?
+                            .kuTimebarGreen : (progress >= 0.2 ?
+                                .kuTimebarOrange : .kuTimebarRed))
                         .padding(.horizontal, 4)
                 }
             }
@@ -41,21 +44,22 @@ struct TimerBarView: View {
     }
     
     private func offsetValue() -> Double {
-        // 90.81% 이상
+        // 90.81% 이상은 100% 상태로 움직이지 않음
         if progress > (248 / 272) {
             return 112
         }
-        // 9.19% 미만
+        // 9.19% 미만부터는 0% 상태로 움직이지 않음
         else if progress < (24 / 272) {
             return -112
         }
-        // 중간
+        // 중간 부분 offset 계산
         else {
             return (272 * progress) - 136
         }
     }
 }
 
+/// 테스트용 뷰
 struct TimerBarTestView: View {
     @State var progress: Double = 1.0
     
@@ -64,6 +68,7 @@ struct TimerBarTestView: View {
             Color.gray.ignoresSafeArea()
             
             VStack {
+                // 뷰의 범위를 알아볼 수 있도록 border 추가
                 TimerBarView(progress: $progress)
                     .border(.black)
                 
