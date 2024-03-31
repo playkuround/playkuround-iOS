873e018acf04c88ed0bf8ab2b9c04c55ea244044
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-04-01 00:42:10 +0900

    [Design] #55 - 달 이미지 추가 및 UI 구현

--- playkuround-iOS/Views/Games/MoonGame/MoonGameView.swift
+++ playkuround-iOS/Views/Games/MoonGame/MoonGameView.swift
@@ -9,32 +9,41 @@ import SwiftUI
 
 struct MoonGameView: View {
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             Image(.moonBackground)
                 .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(minWidth: 0, maxWidth: .infinity, maxHeight: .infinity)
                 .ignoresSafeArea(.all)
             
             VStack {
                 Text(StringLiterals.Game.Moon.description)
                     .font(.pretendard15R)
                     .foregroundStyle(.white)
+                    .padding(.bottom, 10)
+                
+                Text("100")
+                    .font(.neo50)
+                    .kerning(-0.41)
+                    .foregroundStyle(.white)
+                
+                Spacer()
             }
-            
-            Spacer()
-                .customNavigationBar(centerView: {
-                    Text(StringLiterals.Game.Moon.title)
-                        .font(.neo22)
-                        .kerning(-0.41)
-                        .foregroundStyle(.white)
-                }, rightView: {
-                    Button(action: {
-                        // TODO: 일시정지
-                    }, label: {
-                        Image(.yellowPauseButton)
-                    })
-                }, height: 130)
+            .overlay {
+                Image(.moon1)
+                    .padding(.bottom, 50)
+            }
+            .padding(.top, 100)
+            .customNavigationBar(centerView: {
+                Text(StringLiterals.Game.Moon.title)
+                    .font(.neo22)
+                    .kerning(-0.41)
+                    .foregroundStyle(.white)
+            }, rightView: {
+                Button(action: {
+                    // TODO: 일시정지
+                }, label: {
+                    Image(.yellowPauseButton)
+                })
+            }, height: 67)
         }
     }
 }
