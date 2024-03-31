ed8d93f7fd732b5cc0aa55b3af304ebd8b8ad2da
Hoeun Lee <hoeunlee228@gmail.com>
2024-03-31 14:58:38 +0900

    [Design] #56 - GameResultView UI 구현

---
+++ playkuround-iOS/Views/Games/GameResultView.swift
@@ -0,0 +1,75 @@
+//
+//  GameResultView.swift
+//  playkuround-iOS
+//
+//  Created by Hoeun Lee on 3/31/24.
+//
+
+import SwiftUI
+
+struct GameResultView: View {
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.66)
+                .ignoresSafeArea()
+            
+            Image(.resultBackground)
+                .overlay {
+                    VStack {
+                        // TODO: GameViewModel 연결 시 수정
+                        Text("GAME_TITLE")
+                            .font(.neo24)
+                            .foregroundStyle(.kuText)
+                            .kerning(-0.41)
+                        
+                        Image(.scoreBackground)
+                            .overlay {
+                                VStack(alignment: .center) {
+                                    // TODO: 게임 점수 연결 필요
+                                    Text("\(0) " + StringLiterals.Game.Result.score)
+                                        .font(.neo56)
+                                        .foregroundStyle(.kuText)
+                                        .kerning(-0.41)
+                                        .padding(.bottom, 2)
+                                    
+                                    // TODO: 최고 점수 연결 필요
+                                    Text(StringLiterals.Game.Result.bestScore 
+                                         + " \(0) "
+                                         + StringLiterals.Game.Result.score)
+                                        .font(.neo18)
+                                        .foregroundStyle(.kuText)
+                                        .kerning(-0.41)
+                                }
+                                .padding(.top, 48)
+                            }
+                        
+                        // TODO: 모험 점수 연결 필요
+                        Text(StringLiterals.Game.Result.adventureScore 
+                             + " \(0) "
+                             + StringLiterals.Game.Result.score)
+                            .font(.neo24)
+                            .foregroundStyle(.kuText)
+                            .kerning(-0.41)
+                            .padding(.vertical, 20)
+                        
+                        Button {
+                            // TODO: 홈으로 이동
+                        } label: {
+                            Image(.shortButtonBlue)
+                                .overlay {
+                                    Text(StringLiterals.Game.Result.out)
+                                        .font(.neo18)
+                                        .foregroundStyle(.kuText)
+                                        .kerning(-0.41)
+                                }
+                        }
+                    }
+                    .padding(.top, 40)
+                }
+        }
+    }
+}
+
+#Preview {
+    GameResultView()
+}
