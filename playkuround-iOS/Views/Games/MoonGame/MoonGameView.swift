c409ce8f459e9386ef47b64736dd9d0cc9661b56
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-04-01 12:38:33 +0900

    [Design] #55 - GeometryReader를 사용하여 뷰의 사이즈에 따른 Image Padding값 조절

--- playkuround-iOS/Views/Games/MoonGame/MoonGameView.swift
+++ playkuround-iOS/Views/Games/MoonGame/MoonGameView.swift
@@ -9,41 +9,51 @@ import SwiftUI
 
 struct MoonGameView: View {
     var body: some View {
-        ZStack(alignment: .top) {
-            Image(.moonBackground)
-                .resizable()
-                .ignoresSafeArea(.all)
-            
-            VStack {
-                Text(StringLiterals.Game.Moon.description)
-                    .font(.pretendard15R)
-                    .foregroundStyle(.white)
-                    .padding(.bottom, 10)
+        GeometryReader { geometry in
+            ZStack(alignment: .top) {
+                Image(.moonBackground)
+                    .resizable()
+                    .ignoresSafeArea(.all)
                 
-                Text("100")
-                    .font(.neo50)
-                    .kerning(-0.41)
-                    .foregroundStyle(.white)
+                // 뷰의 사이즈에 따라 Image의 padding값 조절
+                let shouldImagePadding = geometry.size.height >= 700
                 
-                Spacer()
+                VStack {
+                    Text(StringLiterals.Game.Moon.description)
+                        .font(.pretendard15R)
+                        .foregroundStyle(.white)
+                        .padding(.bottom, 10)
+                    
+                    Text("100")
+                        .font(.neo50)
+                        .kerning(-0.41)
+                        .foregroundStyle(.white)
+                    
+                    Spacer()
+                }
+                .overlay {
+                    if shouldImagePadding {
+                        Image(.moon4)
+                            .padding(.bottom, 40)
+                    }
+                    else {
+                        Image(.moon4)
+                    }
+                }
+                .padding(.top, 70)
+                .customNavigationBar(centerView: {
+                    Text(StringLiterals.Game.Moon.title)
+                        .font(.neo22)
+                        .kerning(-0.41)
+                        .foregroundStyle(.white)
+                }, rightView: {
+                    Button(action: {
+                        // TODO: 일시정지
+                    }, label: {
+                        Image(.yellowPauseButton)
+                    })
+                }, height: 67)
             }
-            .overlay {
-                Image(.moon1)
-                    .padding(.bottom, 50)
-            }
-            .padding(.top, 100)
-            .customNavigationBar(centerView: {
-                Text(StringLiterals.Game.Moon.title)
-                    .font(.neo22)
-                    .kerning(-0.41)
-                    .foregroundStyle(.white)
-            }, rightView: {
-                Button(action: {
-                    // TODO: 일시정지
-                }, label: {
-                    Image(.yellowPauseButton)
-                })
-            }, height: 67)
         }
     }
 }
