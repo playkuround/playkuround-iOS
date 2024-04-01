eb84e326dc66b029ed14a8e5c64ae4ca0cba5214
SeoYeon Choi <lala1470@konkuk.ac.kr>
2024-04-02 01:00:03 +0900

    [Design] #59 - 폰트 추가 적용 및 타이머, 정답유무 텍스트 추가 구현

--- playkuround-iOS/Views/Games/QuizGame/QuizGameView.swift
+++ playkuround-iOS/Views/Games/QuizGame/QuizGameView.swift
@@ -16,20 +16,35 @@ struct QuizGameView: View {
                 .resizable()
                 .ignoresSafeArea(.all)
             
-            QuizBlockView(quiz: quizData[1])
-                .padding(.top, 140)
-                .customNavigationBar(centerView: {
-                    Text(StringLiterals.Game.Quiz.title)
-                        .font(.neo22)
-                        .kerning(-0.41)
-                        .foregroundStyle(.kuText)
-                }, rightView: {
-                    Button(action: {
-                        // TODO: 일시 중지
-                    }, label: {
-                        Image(.grayPauseButton)
-                    })
-                }, height: 40)
+            VStack {
+                QuizBlockView(quiz: quizData[1])
+                    .padding(.top, 130)
+                
+                Text("00.15")
+                    .font(.neo45)
+                    .kerning(-0.41)
+                    .foregroundStyle(.kuText)
+                    .padding(.top, 30)
+            
+                Text(StringLiterals.Game.Quiz.incorrect)
+                    .font(.pretendard15R)
+                    .foregroundStyle(.kuRed)
+                    .multilineTextAlignment(.center)
+                    
+            }
+            .customNavigationBar(centerView: {
+                Text(StringLiterals.Game.Quiz.title)
+                    .font(.neo22)
+                    .kerning(-0.41)
+                    .foregroundStyle(.kuText)
+            }, rightView: {
+                Button(action: {
+                    // TODO: 일시 중지
+                }, label: {
+                    Image(.grayPauseButton)
+                })
+            }, height: 40)
+        
         }
     }
 }
