e39ee4c49b76b153939387818b595a9f9ef117ed
Hoeun Lee <hoeunlee228@gmail.com>
2024-04-03 13:12:30 +0900

    [Feat] #63 - CardGameViewModel 구현

---
+++ playkuround-iOS/Views/Games/CardGame/CardGameViewModel.swift
@@ -0,0 +1,131 @@
+//
+//  CardGameViewModel.swift
+//  playkuround-iOS
+//
+//  Created by Hoeun Lee on 4/2/24.
+//
+
+import Foundation
+import SwiftUI
+
+final class CardGameViewModel: GameViewModel {
+    @Published var cardList: [CardComponent] = [CardComponent(cardType: .bullCard, cardState: .cover),
+                                                CardComponent(cardType: .bullCard, cardState: .cover),
+                                                CardComponent(cardType: .catCard, cardState: .cover),
+                                                CardComponent(cardType: .catCard, cardState: .cover),
+                                                CardComponent(cardType: .flowerCard, cardState: .cover),
+                                                CardComponent(cardType: .flowerCard, cardState: .cover),
+                                                CardComponent(cardType: .gooseCard, cardState: .cover),
+                                                CardComponent(cardType: .gooseCard, cardState: .cover),
+                                                CardComponent(cardType: .lakeCard, cardState: .cover),
+                                                CardComponent(cardType: .lakeCard, cardState: .cover),
+                                                CardComponent(cardType: .milkCard, cardState: .cover),
+                                                CardComponent(cardType: .milkCard, cardState: .cover),
+                                                CardComponent(cardType: .treeCard, cardState: .cover),
+                                                CardComponent(cardType: .treeCard, cardState: .cover),
+                                                CardComponent(cardType: .turtleCard, cardState: .cover),
+                                                CardComponent(cardType: .turtleCard, cardState: .cover)]
+    
+    private var flippedCardIndex: [Int] = []
+    private var correctCount: Int = 0
+    
+    override func startGame() {
+        super.startGame()
+        super.startTimer()
+    }
+    
+    override func timerDone() {
+        finishGame()
+    }
+    
+    override func finishGame() {
+        gameState = .finish
+        
+        // 최종 점수 계산
+        // 맞춘 카드 쌍 * 5점, 시간 * 2
+        score += correctCount * 5 / 2
+        score += Int(timeRemaining) * 2
+        
+        print("final score: \(score)")
+    
+        // 서버로 점수 업로드
+        uploadResult()
+    }
+    
+    func shuffleCard() {
+        cardList.shuffle()
+    }
+    
+    func coverToDrawing(index: Int) {
+        if flippedCardIndex.count < 2 && cardList[index].cardState == .cover {
+            flippedCardIndex.append(index)
+            cardList[index].cardState = .side
+            let queue = DispatchQueue.main
+            // 카드 옆면에서 0.15초 대기
+            queue.asyncAfter(deadline: .now() + 0.15) {
+                self.cardList[index].cardState = .drawing
+                self.checkCard()
+            }
+        }
+    }
+    
+    private func drawingToCover(index: Int) {
+        if cardList[index].cardState == .drawing {
+            cardList[index].cardState = .side
+            let queue = DispatchQueue.main
+            // 카드 옆면에서 0.15초 대기
+            queue.asyncAfter(deadline: .now() + 0.15) {
+                self.cardList[index].cardState = .cover
+            }
+        }
+    }
+    
+    // 카드 숨기는 함수
+    private func hideCard(index: Int) {
+        withAnimation(.linear(duration: 0.7)) {
+            cardList[index].cardState = .hidden
+        }
+        correctCount += 1
+        
+        if correctCount == 16 {
+            super.pauseOrRestartTimer()
+            super.finishGame()
+        }
+    }
+    
+    // 뒤집은 카드 검사
+    private func checkCard() {
+        if flippedCardIndex.count == 2 {
+            let index1 = flippedCardIndex[0]
+            let index2 = flippedCardIndex[1]
+            
+            // 뒤집은 두 카드가 같은 경우
+            if cardList[index1].cardType == cardList[index2].cardType {
+                // 남은 시간 1초당 1점, 1쌍 1점
+                score += Int(timeRemaining) + 1
+
+                let queue = DispatchQueue.main
+                queue.asyncAfter(deadline: .now()) {
+                    self.flippedCardIndex.removeAll()
+                    self.hideCard(index: index1)
+                    self.hideCard(index: index2)
+                }
+            } 
+            // 다른 경우
+            else {
+                let queue = DispatchQueue.main
+                queue.asyncAfter(deadline: .now() + 0.7) {
+                    self.flippedCardIndex.removeAll()
+                    self.drawingToCover(index: index1)
+                    self.drawingToCover(index: index2)
+                }
+            }
+        }
+    }
+}
+
+struct CardComponent: Identifiable, Hashable {
+    var id = UUID()
+    let cardType: CardType
+    var cardState: CardState
+}
