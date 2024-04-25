530852f4845645d89f798958fd8c5669259179fd
Seoyeon Choi <lala1470@konkuk.ac.kr>
2024-04-26 01:25:46 +0900

    [Fix] #70 - QuizData.json의 배열 수정하여 딜레이 현상 시 어색한 뷰 버그 수정

--- playkuround-iOS/Views/Games/QuizGame/QuizGameView.swift
+++ playkuround-iOS/Views/Games/QuizGame/QuizGameView.swift
@@ -26,61 +26,65 @@ struct QuizGameView: View {
                 VStack {
                     let quiz = quizData[viewModel.randomNumber ?? 0]
                     
-                    Text(quiz.question)
-                        .font(.neo20)
-                        .kerning(-0.41)
-                        .lineSpacing(6)
-                        .foregroundStyle(.kuText)
-                        .multilineTextAlignment(.center)
-                        .padding(.bottom, 20)
-                        .padding(.horizontal, 20)
-                    
-                    ForEach(quiz.options.indices, id: \.self) { index in
-                        BlockView(option: quiz.options[index],
-                                  index: index,
-                                  isCorrect: index == quiz.answer,
-                                  viewModel: viewModel,
-                                  isCorrectAnswer: $viewModel.isCorrectAnswer,
-                                  selectedIndex: $selectedIndex)
-                    }
-                    
-                    if let isCorrectAnswer = viewModel.isCorrectAnswer {
-                        //정답일 때
-                        if isCorrectAnswer {
-                            Text(StringLiterals.Game.Quiz.correct)
-                                .font(.pretendard15R)
-                                .foregroundStyle(.kuGreen)
-                                .multilineTextAlignment(.center)
-                                .padding(.top, 20)
-                                .padding(.bottom, shouldImagePadding ? 0 : 25)
-                        }
-                        else {
-                            //오답일 때
-                            Text("\(viewModel.second).\(viewModel.milliSecond)")
-                                .font(shouldImagePadding ? .neo45 : .neo38)
+                    if let randomNumber = viewModel.randomNumber {
+                        if randomNumber >= 1 {
+                            Text(quiz.question)
+                                .font(.neo20)
                                 .kerning(-0.41)
+                                .lineSpacing(6)
                                 .foregroundStyle(.kuText)
-                                .padding(.vertical, shouldImagePadding ? 20 : 0)
-                                .onReceive(viewModel.timer) { _ in
-                                    if let isCorrect = viewModel.isCorrectAnswer {
-                                        if !isCorrect && viewModel.timerState == .running {
-                                            viewModel.updateTimer2()
-                                            viewModel.updateMilliSecondString()
-                                            
-                                            if viewModel.timeRemaining < 0 {
-                                                viewModel.checkTimerFinished()
-                                                selectedIndex = nil
-                                                viewModel.isCorrectAnswer = nil
+                                .multilineTextAlignment(.center)
+                                .padding(.bottom, 20)
+                                .padding(.horizontal, 20)
+                            
+                            ForEach(quiz.options.indices, id: \.self) { index in
+                                BlockView(option: quiz.options[index],
+                                          index: index,
+                                          isCorrect: index == quiz.answer,
+                                          viewModel: viewModel,
+                                          isCorrectAnswer: $viewModel.isCorrectAnswer,
+                                          selectedIndex: $selectedIndex)
+                            }
+                            
+                            if let isCorrectAnswer = viewModel.isCorrectAnswer {
+                                //정답일 때
+                                if isCorrectAnswer {
+                                    Text(StringLiterals.Game.Quiz.correct)
+                                        .font(.pretendard15R)
+                                        .foregroundStyle(.kuGreen)
+                                        .multilineTextAlignment(.center)
+                                        .padding(.top, 20)
+                                        .padding(.bottom, shouldImagePadding ? 0 : 25)
+                                }
+                                else {
+                                    //오답일 때
+                                    Text("\(viewModel.second).\(viewModel.milliSecond)")
+                                        .font(shouldImagePadding ? .neo45 : .neo38)
+                                        .kerning(-0.41)
+                                        .foregroundStyle(.kuText)
+                                        .padding(.vertical, shouldImagePadding ? 20 : 0)
+                                        .onReceive(viewModel.timer) { _ in
+                                            if let isCorrect = viewModel.isCorrectAnswer {
+                                                if !isCorrect && viewModel.timerState == .running {
+                                                    viewModel.updateTimer2()
+                                                    viewModel.updateMilliSecondString()
+                                                    
+                                                    if viewModel.timeRemaining < 0 {
+                                                        viewModel.checkTimerFinished()
+                                                        selectedIndex = nil
+                                                        viewModel.isCorrectAnswer = nil
+                                                    }
+                                                }
                                             }
                                         }
-                                    }
+                                    
+                                    Text(StringLiterals.Game.Quiz.incorrect)
+                                        .font(.pretendard15R)
+                                        .foregroundStyle(.kuRed)
+                                        .multilineTextAlignment(.center)
+                                        .padding(.bottom, shouldImagePadding ? 0 : 25)
                                 }
-                            
-                            Text(StringLiterals.Game.Quiz.incorrect)
-                                .font(.pretendard15R)
-                                .foregroundStyle(.kuRed)
-                                .multilineTextAlignment(.center)
-                                .padding(.bottom, shouldImagePadding ? 0 : 25)
+                            }
                         }
                     }
                 }
