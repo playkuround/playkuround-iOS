e8888173aca00e5d2691a6d2d0ef7d7f2f922ce4
Guryss <lala1470@konkuk.ac.kr>
2024-04-27 00:45:07 +0900

    [Design] #74 - 수강신청 All 클릭 게임 기본 레이아웃 구현

--- playkuround-iOS/Views/Games/AllClickGame/AllClickGameView.swift
+++ playkuround-iOS/Views/Games/AllClickGame/AllClickGameView.swift
@@ -8,11 +8,101 @@
 import SwiftUI
 
 struct AllClickGameView: View {
+    @ObservedObject var viewModel: AllClickGameViewModel
+    @ObservedObject var rootViewModel: RootViewModel
+    
+    @State private var userText: String = ""
+    @FocusState private var focusField: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                Image(.allClickBackground)
+                    .resizable()
+                    .ignoresSafeArea(.all)
+                
+                VStack {
+                    HStack {
+                        Text(StringLiterals.Game.AllClick.score)
+                            .font(.neo22)
+                            .foregroundStyle(.kuText)
+                            .kerning(-0.41)
+                            .padding(.trailing, 5)
+                        
+                        Text("123")
+                            .font(.neo22)
+                            .foregroundStyle(.kuText)
+                            .kerning(-0.41)
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 2){
+                            Image(.allClickHeart)
+                            Image(.allClickHeart)
+                            Image(.allClickHeart)
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Spacer()
+                    
+                    Rectangle()
+                        .foregroundStyle(.white)
+                        .frame(height: 50)
+                        .overlay {
+                            HStack {
+                                Text(StringLiterals.Game.AllClick.register)
+                                    .font(.neo20)
+                                    .kerning(-0.41)
+                                    .foregroundStyle(.allClickGreen)
+                                
+                                Spacer()
+                                
+                                Image(.allClickWritingBox)
+                                    .overlay(alignment: .leading) {
+                                        TextField(StringLiterals.Game.AllClick.writeSubject, text: $userText)
+                                            .font(.neo18)
+                                            .kerning(-0.41)
+                                            .foregroundStyle(.kuText)
+                                            .padding(.leading, 10)
+                                            .focused($focusField)
+                                    }
+                                
+                                Spacer()
+                                
+                                Image(.allClickRegister)
+                                    .overlay {
+                                        Text(StringLiterals.Game.AllClick.register)
+                                            .font(.neo18)
+                                            .kerning(-0.41)
+                                            .foregroundStyle(.white)
+                                    }
+                            }
+                            .padding(.horizontal, 10)
+                        }
+                        .padding(.bottom, 270)
+                        .padding(.top, -15)
+                        .customNavigationBar(centerView: {
+                            Text(StringLiterals.Game.AllClick.title)
+                                .font(.neo22)
+                                .kerning(-0.41)
+                                .foregroundStyle(.kuText)
+                        }, rightView: {
+                            Button(action: {
+                                
+                            }, label: {
+                                Image(.brownPauseButton)
+                            })
+                        }, height: 67)
+                        .padding(.top, -10)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    AllClickGameView()
+    AllClickGameView(viewModel: AllClickGameViewModel(.allClear, rootViewModel: RootViewModel(), mapViewModel: MapViewModel(), timeStart: 30.0, timeEnd: 0.0, timeInterval: 0.01), rootViewModel: RootViewModel())
 }
+
+
