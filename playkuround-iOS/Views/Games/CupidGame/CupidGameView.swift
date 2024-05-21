f816d152adeebbf63a8f637f9c0d22abf8f710ec
Guryss <lala1470@konkuk.ac.kr>
2024-05-22 01:09:44 +0900

    [Feat] #82 - 게임 시작 전 카운트다운 및 게임 종료 시 점수 반영 구현

--- playkuround-iOS/Views/Games/CupidGame/CupidGameView.swift
+++ playkuround-iOS/Views/Games/CupidGame/CupidGameView.swift
@@ -10,60 +10,60 @@ import SwiftUI
 struct CupidGameView: View {
     @ObservedObject var viewModel: CupidGameViewModel
     @ObservedObject var rootViewModel: RootViewModel
-
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 Image(.cupidBackGround)
                     .resizable()
                     .ignoresSafeArea(.all)
-
+                
                 let shouldImagePadding = geometry.size.height >= 700
-
+                
                 /// 타임바
                 TimerBarView(progress: $viewModel.progress, color: .black)
                     .padding(.bottom, shouldImagePadding ? 580 : 490)
                     .onReceive(viewModel.timer) { _ in
                         viewModel.updateTimer()
                     }
-
+                
                 /// 덕쿠
                 HStack {
                     Image(.cupidDuckkuWhite)
                         .offset(x: -geometry.size.width / 2 + 88)
-
+                    
                     Image(.cupidDuckkuBlack)
                         .offset(x: geometry.size.width / 2 - 88)
                 }
                 .padding(.top, shouldImagePadding ? 50 : 25)
-
+                
                 /// 백그라운드 홍예교
                 Image(.cupidBackgroundBridge)
                     .resizable()
                     .ignoresSafeArea(.all)
-
+                
                 /// 결과 뷰 및 정지버튼
                 VStack {
                     /// BAD일 때
-//                    VStack {
-//                        Image(.cupidHeartBroken)
-//                        Image(.cupidBad)
-//                    }
-//                    .padding(.top, shouldImagePadding ? 150 : 130)
-
+                    //                    VStack {
+                    //                        Image(.cupidHeartBroken)
+                    //                        Image(.cupidBad)
+                    //                    }
+                    //                    .padding(.top, shouldImagePadding ? 150 : 130)
+                    
                     /// PERFECT일 때
-//                    VStack {
-//                        Image(.cupidHeart)
-//                        Image(.cupidPerfect)
-//                    }
-//                    .padding(.top, shouldImagePadding ? 150 : 130)
-
+                    //                    VStack {
+                    //                        Image(.cupidHeart)
+                    //                        Image(.cupidPerfect)
+                    //                    }
+                    //                    .padding(.top, shouldImagePadding ? 150 : 130)
+                    
                     /// GOOD일 때
                     Image(.cupidGood)
                         .padding(.top, shouldImagePadding ? 190 : 160)
-
+                    
                     Spacer()
-
+                    
                     Button(action: {
                         //TODO: viewModel에 정의된 함수 넣기
                     }, label: {
@@ -71,7 +71,7 @@ struct CupidGameView: View {
                             .padding(.bottom, 60)
                     })
                 }
-
+                
                 VStack {}
                     .customNavigationBar(centerView: {
                         Text(StringLiterals.Game.Cupid.title)
@@ -86,7 +86,22 @@ struct CupidGameView: View {
                         })
                     }, height: 67)
                     .padding(.top, -10)
+                
+                if viewModel.isCountdownViewPresented {
+                    CountdownView(countdown: $viewModel.countdown)
+                } else if viewModel.isPauseViewPresented {
+                    GamePauseView(viewModel: viewModel)
+                } else if viewModel.isResultViewPresented {
+                    GameResultView(rootViewModel: rootViewModel, gameViewModel: viewModel)
+                }
+            }
+            .onAppear {
+                viewModel.startCountdown()
             }
         }
     }
+}
+
+#Preview {
+    CupidGameView(viewModel: CupidGameViewModel(.cupid, rootViewModel: RootViewModel(), mapViewModel: MapViewModel(), timeStart: 30.0, timeEnd: 0.0, timeInterval: 0.01), rootViewModel: RootViewModel())
 }
--- playkuround-iOS/Views/Games/CupidGame/CupidGameViewModel.swift
+++ playkuround-iOS/Views/Games/CupidGame/CupidGameViewModel.swift
@@ -9,4 +9,20 @@ import SwiftUI
 
 final class CupidGameViewModel: GameViewModel {
     
+    override func startGame() {
+        super.startGame()
+        super.startTimer()
+    }
+    
+    override func timerDone() {
+        finishGame()
+    }
+    
+    override func finishGame() {
+        gameState = .finish
+        print("final score: \(score)")
+    
+        // 서버로 점수 업로드
+        uploadResult()
+    }
 }
