9109b6bd26c28f3082b86e9a1ada693eafb015b4
Guryss <lala1470@konkuk.ac.kr>
2024-06-05 21:24:59 +0900

    [Add] #112 - TotalRankingView에 필요한 이미지 에셋 파일 추가

---
+++ playkuround-iOS/Views/Home/TotalRanking/TotalRankingInformationView.swift
@@ -0,0 +1,18 @@
+//
+//  TotalRankingInformationView.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 6/5/24.
+//
+
+import SwiftUI
+
+struct TotalRankingInformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TotalRankingInformationView()
+}
---
+++ playkuround-iOS/Views/Home/TotalRanking/TotalRankingView.swift
@@ -0,0 +1,18 @@
+//
+//  TotalRankingView.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 6/5/24.
+//
+
+import SwiftUI
+
+struct TotalRankingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TotalRankingView()
+}
