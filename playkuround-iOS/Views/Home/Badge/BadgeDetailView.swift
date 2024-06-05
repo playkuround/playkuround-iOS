705ef6e709a8372861717cdf76d52b936763bcac
Guryss <lala1470@konkuk.ac.kr>
2024-06-05 15:10:49 +0900

    [Design] #106 - BadgeDetailView UI 구현

---
+++ playkuround-iOS/Views/Home/Badge/BadgeDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  BadgeDetailView.swift
+//  playkuround-iOS
+//
+//  Created by Seoyeon Choi on 6/5/24.
+//
+
+import SwiftUI
+
+struct BadgeDetailView: View {
+    let badge: Badge
+    @Binding var showDetail: Bool
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.5)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    showDetail.toggle()
+                }
+            
+            Image(.badgePopup)
+                .overlay {
+                    VStack {
+                        badge.image
+                            .resizable()
+                            .frame(width: 120, height: 120)
+                            .padding(.bottom, 16)
+                        
+                        Text(badge.title)
+                            .font(.neo20)
+                            .kerning(-0.41)
+                            .foregroundStyle(.kuText)
+                            .padding(.bottom, 16)
+                        
+                        Text(badge.description)
+                            .font(.pretendard15R)
+                            .foregroundStyle(.kuText)
+                            .lineSpacing(1.3)
+                            .multilineTextAlignment(.center)
+                    }
+                    .padding(.horizontal, 48)
+                }
+        }
+    }
+}
