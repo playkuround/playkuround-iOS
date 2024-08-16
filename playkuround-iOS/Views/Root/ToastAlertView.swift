6785f66e836192597a806a37c8d6b09ee938594e
Hoeun Lee <hoeunlee228@gmail.com>
2024-08-16 20:15:33 +0900

    [Design] #141 - 토스트 메시지 창 디자인 구현

---
+++ playkuround-iOS/Views/Root/ToastAlertView.swift
@@ -0,0 +1,32 @@
+//
+//  ToastAlertView.swift
+//  playkuround-iOS
+//
+//  Created by Hoeun Lee on 8/16/24.
+//
+
+import SwiftUI
+
+struct ToastAlertView: View {
+    let alertText: String
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.5).ignoresSafeArea()
+            Image(.toastAlert)
+            HStack(alignment: .center, spacing: 10) {
+                Text(alertText)
+                    .font(.pretendard15R)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.kuText)
+            }
+            .padding(10)
+            .frame(width: 238, height: 100, alignment: .center)
+            
+        }
+    }
+}
+
+#Preview {
+    ToastAlertView(alertText: "서버에 연결할 수 없습니다.")
+}
