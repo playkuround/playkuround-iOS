036acefe4fe3a4844f434a100069da1a55c9c889
Hoeun Lee <hoeunlee228@gmail.com>
2024-08-17 02:13:20 +0900

    [Fix] #141 - 텍스트 overlay로 구조 수정

--- playkuround-iOS/Views/Root/ToastAlertView.swift
+++ playkuround-iOS/Views/Root/ToastAlertView.swift
@@ -14,19 +14,20 @@ struct ToastAlertView: View {
         ZStack {
             Color.black.opacity(0.5).ignoresSafeArea()
             Image(.toastAlert)
-            HStack(alignment: .center, spacing: 10) {
-                Text(alertText)
-                    .font(.pretendard15R)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.kuText)
-            }
-            .padding(10)
-            .frame(width: 238, height: 100, alignment: .center)
-            
+                .overlay {
+                    HStack(alignment: .center, spacing: 10) {
+                        Text(alertText)
+                            .font(.pretendard15R)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.kuText)
+                    }
+                    .padding(10)
+                    .frame(width: 238, height: 100, alignment: .center)
+                }
         }
     }
 }
 
 #Preview {
-    ToastAlertView(alertText: "서버에 연결할 수 없습니다.")
+    ToastAlertView(alertText: "서버에 연결할 수 없습니다.\n잠시 후 다시 시도해주세요. 테스트 테스트 테스트")
 }
