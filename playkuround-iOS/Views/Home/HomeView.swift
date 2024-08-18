22316f37193b192488337b17506c283937faac2b
Hoeun Lee <hoeunlee228@gmail.com>
2024-08-19 01:20:43 +0900

    [Feat] #143 - HomeViewModel에 프로필 뱃지 뷰 상태 추가

--- playkuround-iOS/Views/Home/HomeView.swift
+++ playkuround-iOS/Views/Home/HomeView.swift
@@ -173,6 +173,8 @@ struct HomeView: View {
                     LandmarkView(homeViewModel: homeViewModel)
                 case .adventure:
                     AdventureView(viewModel: viewModel, homeViewModel: homeViewModel)
+                case .badgeProfile:
+                    ProfileBadgeView(homeViewModel: homeViewModel)
                 }
                 
                 if showStoryView {
