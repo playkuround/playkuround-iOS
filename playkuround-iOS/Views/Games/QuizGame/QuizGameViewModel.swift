198636b35ee13184c0e785b2b43b90235ee71313
Guryss <lala1470@konkuk.ac.kr>
2024-08-22 00:46:33 +0900

    [Feat] #155 - 맞춘 문제수에 따른 score 계산 적용

--- playkuround-iOS/Views/Games/QuizGame/QuizGameViewModel.swift
+++ playkuround-iOS/Views/Games/QuizGame/QuizGameViewModel.swift
@@ -64,10 +64,23 @@ final class QuizGameViewModel: GameViewModel {
     
     override func finishGame() {
         gameState = .finish
+        self.calculateScore()
         
         // 3초 뒤 서버로 점수 업로드
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             super.uploadResult(uploadScore: self.score)
+        }
+    }
+    
+    private func calculateScore() {
+        if correctAnswersCount >= 0 && correctAnswersCount <= 9 {
+            score = 10 * ( correctAnswersCount + 1 )
+        }
+        else if correctAnswersCount >= 10 && correctAnswersCount <= 14 {
+            score = 10 * ( 2 * correctAnswersCount - 8 )
+        }
+        else if correctAnswersCount == 15 {
+            score = 250
         }
     }
 }
