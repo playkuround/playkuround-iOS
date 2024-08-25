545c6b66837d79d26965db4bcc857d8092ae8fb5
Hoeun Lee <hoeunlee228@gmail.com>
2024-08-25 16:49:39 +0900

    [Feat] #184 - 뱃지 4종 데이터 추가

--- playkuround-iOS/Views/Home/Badge/BadgeView.swift
+++ playkuround-iOS/Views/Home/Badge/BadgeView.swift
@@ -70,7 +70,7 @@ struct BadgeView: View {
                                 
                                 LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 10), count: 4), spacing: 10) {
                                     /// 11~37번째: 탐험 뱃지
-                                    ForEach(Badge.allCases.suffix(27), id: \.self) { badge in
+                                    ForEach(Badge.allCases.suffix(31), id: \.self) { badge in
                                         filterBadgeImage(for: badge)
                                             .resizable()
                                             .aspectRatio(contentMode: .fit)
