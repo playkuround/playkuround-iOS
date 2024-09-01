7bff0178a986a6948e8ce2f1faa1d6406e0bf2de
Hoeun Lee <hoeunlee228@gmail.com>
2024-09-02 03:20:48 +0900

    [Refactor] #224 - 덕큐피트 QA 반영 리팩토링

--- playkuround-iOS/Views/Games/CupidGame/CupidGameView.swift
+++ playkuround-iOS/Views/Games/CupidGame/CupidGameView.swift
@@ -22,11 +22,20 @@ struct CupidGameView: View {
                 
                 let shouldImagePadding = geometry.size.height >= 700
                 
-                /// 타임바
                 TimerBarView(progress: $viewModel.progress, color: .black)
                     .padding(.bottom, shouldImagePadding ? 580 : 490)
                     .onReceive(viewModel.timer) { _ in
                         viewModel.updateTimer()
+                        
+                        if viewModel.isTimerUpdating {
+                            if Int(viewModel.timeRemaining * 100) % 50 == 0 && Int(viewModel.timeRemaining * 100) > 50 {
+                                viewModel.spawnDuckkus()
+                            }
+                            
+                            if Int(viewModel.timeRemaining * 100) % 4 == 0 {
+                                viewModel.updateEntityPos()
+                            }
+                        }
                     }
                 HStack {
                     Text("SCORE \(viewModel.score)")
@@ -39,42 +48,65 @@ struct CupidGameView: View {
                 }
                 .padding(.leading, 20)
                 
-                /// 덕쿠
-                ForEach(Array(zip(viewModel.whiteDucksPositions.indices, viewModel.whiteDucksPositions)), id: \.0) { index, position in
-                    HStack {
-                        Image(.cupidDuckkuWhite)
-                            .offset(x: position)
+                GeometryReader { geometry in
+                    VStack {
+                        Spacer()
                         
-                        Image(.cupidDuckkuBlack)
-                            .offset(x: viewModel.blackDucksPositions[index])
+                        Spacer().frame(height: 40)
+                        
+                        HStack {
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: 84)
+                        .onAppear {
+                            viewModel.setFrameWidth(geometry.size.width)
+                        }
+                        .overlay {
+                            ZStack {
+                                // black
+                                ForEach(Array(viewModel.duckkuList.enumerated()), id: \.offset) { index, duckku in
+                                    if !duckku.died {
+                                        Image(.cupidDuckkuBlack)
+                                            .offset(x: duckku.posBlack)
+                                    }
+                                }
+                                
+                                // white
+                                ForEach(Array(viewModel.duckkuList.enumerated()), id: \.offset) { index, duckku in
+                                    if !duckku.died {
+                                        Image(.cupidDuckkuWhite)
+                                            .offset(x: duckku.posWhite)
+                                    }
+                                }
+                            }
+                        }
+                        Spacer()
                     }
                 }
-                .padding(.top, shouldImagePadding ? 50 : 25)
                 
-                /// 백그라운드 홍예교
                 Image(.cupidBackgroundBridge)
                     .resizable()
                     .ignoresSafeArea(.all)
                 
-                /// 결과 뷰
                 VStack {
-                    if viewModel.result == .perfect {
-                        VStack {
-                            Image(.cupidHeart)
-                            Image(.cupidPerfect)
+                    if viewModel.isResultTitleShowing {
+                        switch viewModel.resultType {
+                        case .perfect:
+                            VStack {
+                                Image(.cupidHeart)
+                                Image(.cupidPerfect)
+                            }
+                            .padding(.top, shouldImagePadding ? 150 : 130)
+                        case .good:
+                            Image(.cupidGood)
+                                .padding(.top, shouldImagePadding ? 190 : 160)
+                        case .bad:
+                            VStack {
+                                Image(.cupidHeartBroken)
+                                Image(.cupidBad)
+                            }
+                            .padding(.top, shouldImagePadding ? 150 : 130)
                         }
-                        .padding(.top, shouldImagePadding ? 150 : 130)
-                    }
-                    else if viewModel.result == .good {
-                        Image(.cupidGood)
-                            .padding(.top, shouldImagePadding ? 190 : 160)
-                    }
-                    else if viewModel.result == .bad {
-                        VStack {
-                            Image(.cupidHeartBroken)
-                            Image(.cupidBad)
-                        }
-                        .padding(.top, shouldImagePadding ? 150 : 130)
                     }
                     
                     Spacer()
@@ -88,7 +120,7 @@ struct CupidGameView: View {
                     })
                 }
                 
-                VStack {}
+                EmptyView()
                     .customNavigationBar(centerView: {
                         Text("Game.Cupid.Title")
                             .font(.neo22)
@@ -113,19 +145,8 @@ struct CupidGameView: View {
                     CountdownView(countdown: $viewModel.countdown)
                 } else if viewModel.isPauseViewPresented {
                     GamePauseView(viewModel: viewModel)
-                        .onAppear {
-                            viewModel.stopDuckAnimation()
-                            viewModel.stopDuckSpawn()
-                        }
-                        .onDisappear {
-                            viewModel.startDuckAnimation()
-                            viewModel.startDuckSpawn()
-                        }
                 } else if viewModel.isResultViewPresented {
                     GameResultView(rootViewModel: rootViewModel, gameViewModel: viewModel)
-                        .onAppear {
-                            viewModel.stopDuckAnimation()
-                        }
                 } else if viewModel.isWaitingViewPresented {
                     GameWaitingView(second: $viewModel.countdown)
                 }
